3d3a7d494e2fcdd7ae6d36bdf60b15b29c51d5a9
Vladimir Trifonov <trifonov.vp@gmail.com>
2024-01-18 00:28:55 +0200

    wip: fix

--- zkevm-circuits/src/pi_circuit.rs
+++ zkevm-circuits/src/pi_circuit.rs
@@ -314,7 +314,7 @@ impl PublicData {
     }
 
     fn pi_bytes_end_offset(&self) -> usize {
-        self.pi_bytes_start_offset() + N_BYTES_U64 + N_BYTES_WORD * 4
+        self.pi_bytes_start_offset() + N_BYTES_U64 + N_BYTES_WORD * 5 + 8
     }
 
     fn pi_hash_start_offset(&self) -> usize {
@@ -1319,20 +1319,32 @@ impl<F: Field> PiCircuitConfig<F> {
         // Copy data_hash value we collected from assigning data bytes.
         region.constrain_equal(data_hash_rlc_cell.cell(), data_hash_cell.cell())?;
 
-        // let (tmp_offset, _, _, cells) = self.assign_field(
-        //     region,
-        //     offset,
-        //     &public_data.l1_block_range_hash.to_fixed_bytes(),
-        //     RpiFieldType::DefaultType,
-        //     false, // no padding in this case
-        //     rpi_rlc_acc,
-        //     rpi_length,
-        //     challenges,
-        // )?;
-        // offset = tmp_offset;
-        // let l1_block_range_hash_cell = cells[RPI_CELL_IDX].clone();
+        let (tmp_offset, _, _, _) = self.assign_field(
+            region,
+            offset,
+            &public_data.last_applied_l1_block.to_be_bytes().to_vec(),
+            RpiFieldType::DefaultType,
+            false, // no padding in this case
+            rpi_rlc_acc,
+            rpi_length,
+            challenges,
+        )?;
+        offset = tmp_offset;
+
+        let (tmp_offset, _, _, cells) = self.assign_field(
+            region,
+            offset,
+            &public_data.l1_block_range_hash.to_fixed_bytes(),
+            RpiFieldType::DefaultType,
+            false, // no padding in this case
+            rpi_rlc_acc,
+            rpi_length,
+            challenges,
+        )?;
+        offset = tmp_offset;
+        let l1_block_range_hash_cell = cells[RPI_CELL_IDX].clone();
         
-        // region.constrain_equal(l1_block_range_hash_rlc_cell.cell(), l1_block_range_hash_cell.cell())?;
+        region.constrain_equal(l1_block_range_hash_rlc_cell.cell(), l1_block_range_hash_cell.cell())?;
 
         let pi_bytes_rlc = cells[RPI_RLC_ACC_CELL_IDX].clone();
         let pi_bytes_length = cells[RPI_LENGTH_ACC_CELL_IDX].clone();
