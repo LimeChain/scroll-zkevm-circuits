3e8dd35baf343b70d299cd8e73c965d690f74603
Vladimir Trifonov <trifonov.vp@gmail.com>
2024-01-25 17:56:41 +0200

    wip: fix

--- bus-mapping/src/evm/opcodes/sload.rs
+++ bus-mapping/src/evm/opcodes/sload.rs
@@ -61,7 +61,8 @@ impl Opcode for Sload {
         // Storage read
         let value_from_statedb = *state.sdb.get_storage(&contract_addr, &key).1;
         {
-            println!("sload: value_from_statedb: {:?}", key.to_string());
+            println!("sload: contract_addr: {:?}", contract_addr.to_string());
+            println!("sload: key -> value_from_statedb: {:?}", &key);
             let value_from_step = geth_step.storage.get_or_err(&key)?;
             let value_from_stack = geth_steps[1].stack.last().unwrap();
             if !(value_from_step == value_from_statedb && value_from_step == value_from_stack) {
